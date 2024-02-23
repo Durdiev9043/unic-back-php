002166d437a5e693ba0cba577c73bb528ddca0d6
durdiev9043 <donyorbek9043@gmail.com>
2024-02-23 21:43:40 +0500

    finished

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,7 +10,7 @@ class TaskController extends Controller
 
     public function index()
     {
-        $tasks=Task::all()->paginate(15);
+        $tasks=Task::paginate(15);
         return view('task.index',['tasks'=>$tasks]);
     }
 
