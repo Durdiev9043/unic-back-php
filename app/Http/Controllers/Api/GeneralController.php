206637fb0e21f6456a8e9e72458366ac6627c0e6
durdiev9043 <donyorbek9043@gmail.com>
2024-03-18 18:41:21 +0500

    finished

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -270,12 +270,13 @@ public function workTime(){
     $atvet=[];
     foreach ($data as $item) {
             $user= DB::table('users')->select('id','name')->where('id',$item->user_id)->first();
+            $region=DB::table('regions')->select('id','name')->where('id',$user->region_id)->first();
         $atvet[$item->id]['id'] = $item->id;
         $atvet[$item->id]['user'] =$user->name;
 
 //        $atvet[$item->id]['user'] = User::where('id',$item->user_id)->first()->name;
-        $atvet[$item->id]['region'] = DB::table('regions')->select('id','name')->where('id',$user->region_id)->first()->name;
-        $atvet[$item->id]['district'] = $item->user->district->name;
+        $atvet[$item->id]['region'] = $region->name;
+//        $atvet[$item->id]['district'] = $item->user->district->name;
         $atvet[$item->id]['day'] = $item->day;
         $atvet[$item->id]['lat'] = $item->latt;
         $atvet[$item->id]['lang'] = $item->lang;
