273a1b0fca9bca94b1695284a6926bfc625c45c8
durdiev9043 <donyorbek9043@gmail.com>
2024-03-18 12:15:58 +0500

    finished

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -270,7 +270,7 @@ public function workTime(){
     $atvet=[];
     foreach ($data as $item) {
         $atvet[$item->id]['id'] = $item->id;
-        $atvet[$item->id]['user'] = $item->user['name'];
+        $atvet[$item->id]['user'] = $item->user->name;
 //        $atvet[$item->id]['user'] = User::where('id',$item->user_id)->first()->name;
 //        $atvet[$item->id]['region'] = $item->user->region->name;
 //        $atvet[$item->id]['district'] = $item->user->district->name;
--- app/Models/Daily.php
+++ app/Models/Daily.php
@@ -12,6 +12,6 @@ class Daily extends Model
 
 
     public function user(){
-        return $this->belongsTo( \App\Models\User::class);
+        return $this->belongsTo( App\Models\User::class);
     }
 }
