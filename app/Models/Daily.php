6f53842eb03e31af2c558efa93338a8c17acb9e0
durdiev9043 <donyorbek9043@gmail.com>
2024-03-18 11:56:03 +0500

    finished

--- app/Models/Daily.php
+++ app/Models/Daily.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 
 class Daily extends Model
 {
@@ -13,6 +12,6 @@ class Daily extends Model
 
 
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo( \App\Models\User::class);
     }
 }
